d395c5e992b355060d62f1045f6dd64f17349de9
sayash chaudhary <sayashchaudhary@gmail.com>
2020-01-16 00:46:31 +0530

    fix

--- src/components/Auth/Register/Register.tsx
+++ src/components/Auth/Register/Register.tsx
@@ -4,34 +4,34 @@ import Radio from '@material-ui/core/Radio'
 import {Formik, Form, useField, FieldAttributes} from 'formik';
 import * as Yup from 'yup';
 import Logo from '../../../assets/images/logo.png';
-import { FormControlLabel } from '@material-ui/core';
+import {FormControlLabel} from '@material-ui/core';
 
-type MyRadioProps = {label:string} & FieldAttributes<{}> 
+type MyRadioProps = { label: string } & FieldAttributes<{}>
 
 const MyRadio: React.FC<MyRadioProps> = ({label, ...props}) => {
     const [field] = useField(props);
-    
+
     return (
-        <FormControlLabel {...field} control={<Radio />} label={label} className={"input1"} />
+        <FormControlLabel {...field} control={<Radio/>} label={label} className={"input1"}/>
     )
-}
+};
 
 
-const MyInputField: React.FC<FieldAttributes<{}>> = ({placeholder,type, ...props}) => {
-    const [field,meta] = useField<{}>(props);
+const MyInputField: React.FC<FieldAttributes<{}>> = ({placeholder, type, ...props}) => {
+    const [field, meta] = useField<{}>(props);
     const errorText = meta.error && meta.touched ? meta.error : '';
-    return(
-        <Input placeholder={placeholder} {...field} type={type} error={!!errorText} className={"input"} />
+    return (
+        <Input placeholder={placeholder} {...field} type={type} error={!!errorText} className={"input"}/>
     )
-}
+};
 
-const validationSchema=Yup.object({
+const validationSchema = Yup.object({
     Name: Yup.string().required('First Name is required'),
     Email: Yup.string().email('Email is invalid').required('Email is required'),
     password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
     cnfPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Confirm Password is required'),
     phoneno: Yup.number().min(10, 'Contact number should be at least 10 digits').required('Contact number is required'),
-    gender:Yup.string().required('Gender is required')
+    gender: Yup.string().required('Gender is required')
 });
 
 const Register: React.FC = () => {
@@ -41,28 +41,29 @@ const Register: React.FC = () => {
             <div className='outer-card'>
                 <img src={Logo} alt="logo" className="logo"/>
                 <div className='inner-card'>
-                    <Formik 
+                    <Formik
                         validateOnChange={true}
                         validationSchema={validationSchema}
-                        initialValues={{ Name:"",Email:"",password:"",cnfPassword:"",phoneno:"",gender:""}} onSubmit={(data, {setSubmitting}) => {
+                        initialValues={{Name: "", Email: "", password: "", cnfPassword: "", phoneno: "", gender: ""}}
+                        onSubmit={(data, {setSubmitting}) => {
                             setSubmitting(true);
                             console.log(data);
                             setSubmitting(false);
-                    }}>
+                        }}>
                         <Form>
-                            <MyInputField name="Name" type="text" placeholder="Your Name" />
-                            <MyInputField name="Email" type="email" placeholder="E-mail" />
-                            <MyInputField name="password" type="password" placeholder="Password" />
-                            <MyInputField name="cnfPassword" type="password" placeholder="Confirm Password" />
-                            <MyInputField name="phoneno" type="input" placeholder="Phone Number" />
-                            <MyRadio name="gender" type="radio" value="male" label="Male" />
-                            <MyRadio name="gender" type="radio" value="female" label="Female" />
-                            <button  type="submit" className="submit">Submit</button>
+                            <MyInputField name="Name" type="text" placeholder="Your Name"/>
+                            <MyInputField name="Email" type="email" placeholder="E-mail"/>
+                            <MyInputField name="password" type="password" placeholder="Password"/>
+                            <MyInputField name="cnfPassword" type="password" placeholder="Confirm Password"/>
+                            <MyInputField name="phoneno" type="input" placeholder="Phone Number"/>
+                            <MyRadio name="gender" type="radio" value="male" label="Male"/>
+                            <MyRadio name="gender" type="radio" value="female" label="Female"/>
+                            <button type="submit" className="submit">Submit</button>
                         </Form>
                     </Formik>
                 </div>
             </div>
-        </div>            
+        </div>
     )
 };
 
