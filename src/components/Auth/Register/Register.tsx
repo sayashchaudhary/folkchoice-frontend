8654145e3316330d9d169d164a772f1e8af51b6e
shikhar1111 <shikhar4frnds.agarwal11@gmail.com>
2020-01-16 00:26:36 +0530

    added forms through formik

--- src/components/Auth/Register/Register.tsx
+++ src/components/Auth/Register/Register.tsx
@@ -1,84 +1,68 @@
 import React from 'react';
-import Input from '@material-ui/core/Input';
-import {Formik, Field, Form, ErrorMessage} from 'formik';
+import Input from '@material-ui/core/Input'
+import Radio from '@material-ui/core/Radio'
+import {Formik, Form, useField, FieldAttributes} from 'formik';
 import * as Yup from 'yup';
 import Logo from '../../../assets/images/logo.png';
+import { FormControlLabel } from '@material-ui/core';
+
+type MyRadioProps = {label:string} & FieldAttributes<{}> 
+
+const MyRadio: React.FC<MyRadioProps> = ({label, ...props}) => {
+    const [field] = useField(props);
+    
+    return (
+        <FormControlLabel {...field} control={<Radio />} label={label} className={"input1"} />
+    )
+}
+
+
+const MyInputField: React.FC<FieldAttributes<{}>> = ({placeholder,type, ...props}) => {
+    const [field,meta] = useField<{}>(props);
+    const errorText = meta.error && meta.touched ? meta.error : '';
+    return(
+        <Input placeholder={placeholder} {...field} type={type} error={!!errorText} className={"input"} />
+    )
+}
+
+const validationSchema=Yup.object({
+    Name: Yup.string().required('First Name is required'),
+    Email: Yup.string().email('Email is invalid').required('Email is required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+    cnfPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Confirm Password is required'),
+    phoneno: Yup.number().min(10, 'Contact number should be at least 10 digits').required('Contact number is required'),
+    gender:Yup.string().required('Gender is required')
+});
 
 const Register: React.FC = () => {
     return (
-        <Formik
-            initialValues={{
-                yourName: '',
-                email: '',
-                password: '',
-                confirmPassword: '',
-                contactNumber: ''
-            }}
-            validationSchema={Yup.object().shape({
-                yourName: Yup.string()
-                    .required('First Name is required'),
-                email: Yup.string()
-                    .email('Email is invalid')
-                    .required('Email is required'),
-                password: Yup.string()
-                    .min(6, 'Password must be at least 6 characters')
-                    .required('Password is required'),
-                confirmPassword: Yup.string()
-                    .oneOf([Yup.ref('password'), null], 'Passwords must match')
-                    .required('Confirm Password is required'),
-                contactNumber: Yup.string()
-                    .min(10, 'Contact number should be at least 10 digits')
-                    .required('Contact number is required')
-            })}
-            onSubmit={fields => {
-                alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
-            }}
-            render={({errors, status, touched}) => (
-                <Form>
-                    <div className='container-fluid'>
-                        <div className='background'/>
-                        <div className='outer-card'>
-                            <img src={Logo} alt="logo" className="logo"/>
-                            <div className='inner-card'>
-                                <div className="form-group">
-                                    <Input name="firstName" type="text" placeholder="First Name"
-                                           className={'form-control' + (errors.yourName && touched.yourName ? ' is-invalid' : '')}/>
-                                    <ErrorMessage name="yourName" component="div" className="invalid-feedback"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <Field name="email" type="text"
-                                           className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')}/>
-                                    <ErrorMessage name="email" component="div" className="invalid-feedback"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <Field name="password" type="password"
-                                           className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')}/>
-                                    <ErrorMessage name="password" component="div" className="invalid-feedback"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="confirmPassword">Confirm Password</label>
-                                    <Field name="confirmPassword" type="password"
-                                           className={'form-control' + (errors.confirmPassword && touched.confirmPassword ? ' is-invalid' : '')}/>
-                                    <ErrorMessage name="confirmPassword" component="div" className="invalid-feedback"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="contactNumber">Contact Number</label>
-                                    <Field name="contactNumber" type="number"
-                                           className={'form-control' + (errors.contactNumber && touched.contactNumber ? ' is-invalid' : '')}/>
-                                    <ErrorMessage name="contactNumber" component="div" className="invalid-feedback"/>
-                                </div>
-                                <div className="form-group">
-                                    <button type="submit" className="btn btn-primary mr-2">Register</button>
-                                    <button type="reset" className="btn btn-secondary">Reset</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Form>
-            )}
-        />
+        <div className='container-fluid'>
+            <div className='background'/>
+            <div className='outer-card'>
+                <img src={Logo} alt="logo" className="logo"/>
+                <div className='inner-card'>
+                    <Formik 
+                        validateOnChange={true}
+                        validationSchema={validationSchema}
+                        initialValues={{ Name:"",Email:"",password:"",cnfPassword:"",phoneno:"",gender:""}} onSubmit={(data, {setSubmitting}) => {
+                            setSubmitting(true);
+                            console.log(data);
+                            setSubmitting(false);
+                    }}>
+                        <Form>
+                            <MyInputField name="Name" type="text" placeholder="Your Name" />
+                            <MyInputField name="Email" type="email" placeholder="E-mail" />
+                            <MyInputField name="password" type="password" placeholder="Password" />
+                            <MyInputField name="cnfPassword" type="password" placeholder="Confirm Password" />
+                            <MyInputField name="phoneno" type="input" placeholder="Phone Number" />
+                            <MyRadio name="gender" type="radio" value="male" label="Male" />
+                            <MyRadio name="gender" type="radio" value="female" label="Female" />
+                            <button  type="submit" className="submit">Submit</button>
+                        </Form>
+                    </Formik>
+                </div>
+            </div>
+        </div>            
     )
 };
 
