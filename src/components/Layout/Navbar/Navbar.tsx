2c87a8f79621a7474733e1c1da709e59dec91213
naimishverma17 <itakgec@gmail.com>
2020-01-31 02:05:59 +0530

    indentation

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -17,53 +17,54 @@ import { RouteUtils } from '../../../routing/route-utils';
 
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      width: theme.spacing(7),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      zIndex:100,
-      color:'#373A59'
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 7),
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: 200,
-      },
-      color:'#373A59',
-    },
-  }),
+    createStyles({
+        search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: fade(theme.palette.common.white, 0.15),
+            '&:hover': {
+                backgroundColor: fade(theme.palette.common.white, 0.25),
+            },
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                marginLeft: theme.spacing(3),
+                width: 'auto',
+            },
+        },
+        searchIcon: {
+            width: theme.spacing(7),
+            height: '100%',
+            position: 'absolute',
+            pointerEvents: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: 100,
+            color: '#373A59'
+        },
+        inputInput: {
+            padding: theme.spacing(1, 1, 1, 7),
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('md')]: {
+                width: 200,
+            },
+            color: '#373A59',
+        },
+    }),
 );
 
-export default function PrimarySearchAppBar() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
-  const isMenuOpen = Boolean(anchorEl);
+const PrimarySearchAppBar = () => {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
-  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
     const handleMenuClose = () => {
         setAnchorEl(null);
@@ -75,60 +76,68 @@ export default function PrimarySearchAppBar() {
         }
     };
 
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      id={menuId}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <div className="menu_items">
-        <MenuItem onClick={handleMenuClose} ><Link to="/"><img src={User} alt="User" className="menu_items__user" />USER</Link></MenuItem>
-        <MenuItem onClick={handleMenuClose} ><Link to="/">My Orders</Link></MenuItem>
-        <MenuItem onClick={handleMenuClose} ><Link to="/">My Cart</Link></MenuItem>
-        <MenuItem onClick={handleMenuClose} ><Link to="/">Saved Items</Link></MenuItem>
-        <MenuItem onClick={handleMenuClose} ><Link to="/">Customer Support</Link></MenuItem>
-        <MenuItem onClick={handleMenuClose} ><Link to="/">F.A.Q</Link></MenuItem>
-        <MenuItem onClick={handleMenuClose} ><Link to="/">About</Link></MenuItem>
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            id={menuId}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <div className="menu_items">
+                <MenuItem onClick={handleMenuClose}><Link to="/"><img src={User} alt="User"
+                                                                      className="menu_items__user"/>USER</Link></MenuItem>
+                <MenuItem onClick={handleMenuClose}><Link to="/">My Orders</Link></MenuItem>
+                <MenuItem onClick={handleMenuClose}><Link to="/">My Cart</Link></MenuItem>
+                <MenuItem onClick={handleMenuClose}><Link to="/">Saved Items</Link></MenuItem>
+                <MenuItem onClick={handleMenuClose}><Link to="/">Customer Support</Link></MenuItem>
+                <MenuItem onClick={handleMenuClose}><Link to="/">F.A.Q</Link></MenuItem>
+                <MenuItem onClick={handleMenuClose}><Link to="/">About</Link></MenuItem>
 
-      </div>
-    </Menu>
-  );
+            </div>
+        </Menu>
+    );
 
-  return (
-    <div className="container-fluid">
-        <div className="nav">
-            <AppBar position="static">
-                <Toolbar>
-                    <img src={Hamburger} alt="Hamburger" className="nav__hamburger" onClick={handleProfileMenuOpen}/>
-                    <img src={Logo} alt="logo" className="nav__name"/>
-                    <span className="nav__text">FOLK CHOICE</span>
-                    <div className="nav__search">
-                    <div className={classes.search}>
-                      <div className={classes.searchIcon}>
-                        <SearchIcon />
-                      </div>
-                      <InputBase
-                        placeholder="Search…"
-                        classes={{
-                          input: classes.inputInput,
-                        }}
-                        inputProps={{ 'aria-label': 'search' }}
-                      />
-                    </div>
-                    </div>
-                    <img src={User} alt="User" className="nav__user" />
-                    <img src={Heart} alt="Wishlist" className="nav__heart" />
-                    <img src={Cart} alt="cart" className="nav__cart" />
-                    <div className="nav__right">
-                        <button onClick={() => navigateToRoute(RoutePath.login)} type="submit" className="nav__right-login">Sign in</button>
-                        <button onClick={() => navigateToRoute(RoutePath.register)} type="submit" className="nav__right-signin">Sign up</button>
-                    </div>
+    return (
+        <div className="container-fluid">
+            <div className="nav">
+                <AppBar position="static">
+                    <Toolbar>
+                        <img src={Hamburger} alt="Hamburger" className="nav__hamburger"
+                             onClick={handleProfileMenuOpen}/>
+                        <img src={Logo} alt="logo" className="nav__name"/>
+                        <span className="nav__text">FOLK CHOICE</span>
+                        <div className="nav__search">
+                            <div className={classes.search}>
+                                <div className={classes.searchIcon}>
+                                    <SearchIcon/>
+                                </div>
+                                <InputBase
+                                    placeholder="Search…"
+                                    classes={{
+                                        input: classes.inputInput,
+                                    }}
+                                    inputProps={{ 'aria-label': 'search' }}
+                                />
+                            </div>
+                        </div>
+                        <img src={User} alt="User" className="nav__user"/>
+                        <img src={Heart} alt="Wishlist" className="nav__heart"/>
+                        <img src={Cart} alt="cart" className="nav__cart"/>
+                        <div className="nav__right">
+                            <button onClick={() => navigateToRoute(RoutePath.login)} type="submit"
+                                    className="nav__right-login">Sign in
+                            </button>
+                            <button onClick={() => navigateToRoute(RoutePath.register)} type="submit"
+                                    className="nav__right-signin">Sign up
+                            </button>
+                        </div>
                     </Toolbar>
-            </AppBar>
-            {renderMenu}
+                </AppBar>
+                {renderMenu}
+            </div>
         </div>
-      </div>
-  );
-}
+    );
+};
+
+export default PrimarySearchAppBar;
