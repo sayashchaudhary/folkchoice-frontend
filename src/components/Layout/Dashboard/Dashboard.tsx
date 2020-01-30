91191ce57a2917f6ac4750ca92b3663e1bd94a86
srinya2598 <srinya2598@gamil.com>
2020-01-31 00:01:15 +0530

    props

--- src/components/Layout/Dashboard/Dashboard.tsx
+++ src/components/Layout/Dashboard/Dashboard.tsx
@@ -13,27 +13,57 @@ const Dashboard: React.FC = () => {
                     <div className="dashboard__items-item">
                         <div className="row">
                             <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                                <ProductCard/>
+                                <ProductCard
+                                    name="Lakme 9 to 5 CC - Honey"
+                                    Seller="Seller - Lalita Sales"
+                                    Oldprice="₹521"
+                                    Newprice="₹349"
+                                />
                             </div>
 
                             <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                                <ProductCard/>
+                                <ProductCard
+                                    name="Lakme 9 to 5 CC - Honey"
+                                    Seller="Seller - Lalita Sales"
+                                    Oldprice="₹521"
+                                    Newprice="₹349"
+                                />
                             </div>
 
                             <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                                <ProductCard/>
+                                <ProductCard
+                                    name="Lakme 9 to 5 CC - Honey"
+                                    Seller="Seller - Lalita Sales"
+                                    Oldprice="₹521"
+                                    Newprice="₹349"
+                                />
                             </div>
 
                             <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                                <ProductCard/>
+                                <ProductCard
+                                    name="Lakme 9 to 5 CC - Honey"
+                                    Seller="Seller - Lalita Sales"
+                                    Oldprice="₹521"
+                                    Newprice="₹349"
+                                />
                             </div>
 
                             <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                                <ProductCard/>
+                                <ProductCard
+                                    name="Lakme 9 to 5 CC - Honey"
+                                    Seller="Seller - Lalita Sales"
+                                    Oldprice="₹521"
+                                    Newprice="₹349"
+                                />
                             </div>
 
                             <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                                <ProductCard/>
+                                <ProductCard
+                                    name="Lakme 9 to 5 CC - Honey"
+                                    Seller="Seller - Lalita Sales"
+                                    Oldprice="₹521"
+                                    Newprice="₹349"
+                                />
                             </div></div>
 
                 </div>
@@ -43,27 +73,58 @@ const Dashboard: React.FC = () => {
                     </div>
                     <div className="row">
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                            <ProductCard/>
+                            <ProductCard
+                                name="Lakme 9 to 5 CC - Honey"
+                                Seller="Seller - Lalita Sales"
+                                Oldprice="₹521"
+                                Newprice="₹349"
+                            />
                         </div>
 
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                            <ProductCard/>
+                            <ProductCard
+                                product-image="https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/8/3/83a5d0d8901030655012_g.jpg"
+                                product-name="Lakme 9 to 5 CC - Honey"
+                                product-seller="Seller - Lalita Sales"
+                                product-old-price="₹521"
+                                product-new-price="₹349"
+                            />
                         </div>
 
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                            <ProductCard/>
+                            <ProductCard
+                                name="Lakme 9 to 5 CC - Honey"
+                                Seller="Seller - Lalita Sales"
+                                Oldprice="₹521"
+                                Newprice="₹349"
+                            />
                         </div>
 
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                            <ProductCard/>
+                            <ProductCard
+                                name="Lakme 9 to 5 CC - Honey"
+                                Seller="Seller - Lalita Sales"
+                                Oldprice="₹521"
+                                Newprice="₹349"
+                            />
                         </div>
 
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                            <ProductCard/>
+                            <ProductCard
+                                name="Lakme 9 to 5 CC - Honey"
+                                Seller="Seller - Lalita Sales"
+                                Oldprice="₹521"
+                                Newprice="₹349"
+                            />
                         </div>
 
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2 text-center">
-                            <ProductCard/>
+                            <ProductCard
+                                name="Lakme 9 to 5 CC - Honey"
+                                Seller="Seller - Lalita Sales"
+                                Oldprice="₹521"
+                                Newprice="₹349"
+                            />
                         </div></div>
                 </div>
             </div>
--- src/components/Layout/ProductCard/ProductCard.tsx
+++ src/components/Layout/ProductCard/ProductCard.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const ProductCard: React.FC = (props) => {
+const ProductCard: React.FC = (props:any) => {
     return (
         <div className="container-fluid">
             <div className="dashboard__items">
 
                             <div className="dashboard__items-item__card">
                                 <img src="https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/8/3/83a5d0d8901030655012_g.jpg" alt="img" className="dashboard__items-item__card-image" />
-                                <h4 className="dashboard__items-item__card-name">Lakme 9 to 5 CC - Honey</h4>
-                                <p className="dashboard__items-item__card-seller">Seller - Lalita Sales</p>
-                                <p className="dashboard__items-item__card-seller"><del> ₹521</del><span className="dashboard__items-item__card-seller__price"> ₹349</span> </p>
+                                <h4 className="dashboard__items-item__card-name">{props.name}</h4>
+                                <p className="dashboard__items-item__card-seller">{props.seller}</p>
+                                <p className="dashboard__items-item__card-seller"><del> {props.Oldprice}</del><span className="dashboard__items-item__card-seller__price"> {props.Newprice}</span> </p>
                             </div>
                         </div>
                         </div>
