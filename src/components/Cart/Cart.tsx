a25183998d1c3930763bc7b76fed7d1f8a5fa825
shikhar1199 <shikharagarwal2807@gmail.com>
2020-03-12 23:14:50 +0530

    cart responsive

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -12,7 +12,7 @@ const Cosmetics: React.FC = () => {
                         <h4>DELIVERY ADDRESS :</h4>
                     </div>
                     <div className="col-md-8">
-                        <p>dfsdudifhsifdodnjnijdnunununhujnounufhh</p>
+                        <p>dfsdudifhsifdodnjnijnufhh</p>
                     </div>
                 </div>
             </div>
@@ -23,7 +23,7 @@ const Cosmetics: React.FC = () => {
                              src="https://images-na.ssl-images-amazon.com/images/I/71ivJqXSWVL._AC_UX679_.jpg"
                              className="order__img"/>
                     </div>
-                    <div className="col-md-8">
+                    <div className="col-md-8 spacing">
                         <h3>Women Chikan Embroidery Poly Georgette Straight Kurta (Green) </h3>
                         <br/>
                         <p><b>SOLD BY:</b> RAMA TRADERS</p>
@@ -34,7 +34,7 @@ const Cosmetics: React.FC = () => {
                         <button type="submit" className="order__size">L</button>
                         <button type="submit" className="order__size">XL</button>
                         <button type="submit" className="order__size">XXL</button>
-                        <p><b>QUANTITY</b></p>
+                        <p className="quantity"><b>QUANTITY</b></p>
                         <button type="submit" className="order__add">-</button>
                         <button type="submit" className="order__quantity">1</button>
                         <button type="submit" className="order__add">+</button>
@@ -46,37 +46,39 @@ const Cosmetics: React.FC = () => {
                     <div className="col-12">
                         <h6>Offer/Promocode Applied or Applicable</h6>
                     </div>
-                    <div className="col-md-6">
-                        <p>Price Details :</p>
-                        <p>Discount :</p>
-                        <p>Savings :</p>
-                        <p>Taxes :</p>
-                        <p>Amount Payable :</p>
-                        <br/>
-                        <p>Coupon Code :</p>
-                    </div>
-                    <div className="col-md-6 right">
-                        <p>1499$</p>
-                        <p>-240$</p>
-                        <p>350$</p>
-                        <p>12$</p>
-                        <h4>1271$</h4>
-                        <br/>
-                        <p>QWERTY -20% OFF ON CLOTHING</p>
+                    <div className="col-md-12">
+                        <div className="left">
+                            <p>Price:</p>
+                            <p>Discount:</p>
+                            <p>Savings:</p>
+                            <p>Taxes:</p>
+                            <p>Total:</p>
+                            <br/>
+                            <p>Coupon:</p>
+                        </div>
+                        <div className="right">
+                            <p>1499$</p>
+                            <p>-240$</p>
+                            <p>350$</p>
+                            <p>12$</p>
+                            <h4>1271$</h4>
+                            <br/>
+                            <p>QWERTY -20% OFF ON CLOTHING</p>
+                        </div>
                     </div>
                     <div className="col-12">
-                        <h6>Have A Promocode?</h6>
+                        <h6>Have A Promocode?</h6><br/>
                         <h6>Buying For Someone Else?</h6>
                     </div>
                 </div>
             </div>
-            <div className="container cart">
+            <div className="container cart1">
                 <div className="row">
                     <div className="col-md-8">
                         <h4>TOTAL AMOUNT TO BE PAID:<b>1271$</b></h4>
                     </div>
                     <div className="col-md-4">
-                        <button type="submit" className="cart__button">PLACE ORDER </button>
+                        <button type="submit" className="cart__button">PLACE ORDER</button>
                     </div>
                 </div>
             </div>
