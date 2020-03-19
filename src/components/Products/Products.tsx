c8bd928de83cc9b25866f73f7d7b2a74141ce6ea
shikhar1199 <shikharagarwal2807@gmail.com>
2020-03-20 02:37:11 +0530

    products page along with filter

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+import CenteredModal from "../Shared/Modal/Modal";
+
+const Landing: React.FC = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+    return (
+        <div className="container-fluid">
+            <Navbar/>
+            <div className="products-container">
+                <div className="row ">
+                    <div className="col-md-3">
+                        <div className="panel">
+                            <div className="content">
+                                <div className="row">
+                                    <div className="text col-md-10">
+                                        <h4>Color:</h4>
+                                    </div>
+                                    <div className="drop col-md-2">
+                                        <DropdownButton id="dropdown-basic-button" title="">
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item>
+                                                <button type="submit" className="view"
+                                                        onClick={() => setModalShow(true)}>View More
+                                                </button>
+                                            </Dropdown.Item>
+                                        </DropdownButton>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="content">
+                                <div className="row">
+                                    <div className="text col-md-10">
+                                        <h4>Fabric:</h4>
+                                    </div>
+                                    <div className="drop col-md-2">
+                                        <DropdownButton id="dropdown-basic-button" title="">
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item>
+                                                <button type="submit" className="view"
+                                                        onClick={() => setModalShow(true)}>View More
+                                                </button>
+                                            </Dropdown.Item>
+                                        </DropdownButton>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="content">
+                                <div className="row">
+                                    <div className="text col-md-10">
+                                        <h4>Size:</h4>
+                                    </div>
+                                    <div className="drop col-md-2">
+                                        <DropdownButton id="dropdown-basic-button" title="">
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item>
+                                                <button type="submit" className="view"
+                                                        onClick={() => setModalShow(true)}>View More
+                                                </button>
+                                            </Dropdown.Item>
+                                        </DropdownButton>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="content">
+                                <div className="row">
+                                    <div className="text col-md-10">
+                                        <h4>Other Filter:</h4>
+                                    </div>
+                                    <div className="drop col-md-2">
+                                        <DropdownButton id="dropdown-basic-button" title="">
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                            <Dropdown.Item href="#/action-2">Action</Dropdown.Item>
+                                            <Dropdown.Item>
+                                                <button type="submit" className="view"
+                                                        onClick={() => setModalShow(true)}>View More
+                                                </button>
+                                            </Dropdown.Item>
+                                        </DropdownButton>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-md-9">
+                        <h1><b>Products Title here.....</b></h1>
+                        <CenteredModal
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+
+export default Landing;
